24b15cb16f3db5e62d7d194a68ad8343116aa310
schalalp <123514074+schalalp@users.noreply.github.com>
2026-09-09 01:19:17 -0300

    Update secundariojogo.py

--- secundariojogo.py
+++ secundariojogo.py
@@ -1,5 +1,69 @@
-for moeda in self.moedas:
+import pyxel
+
+class Jogo: 
+    def __init__(self): 
+        pyxel.init(160, 120, title="Eiffel Up", fps=10) 
+        pyxel.load("eiffel_up.pyxres") 
+        #tentativa png
+        ##pyxel.images[1].load(65,71, "gabriel2.png")
+
+        # Balão
+        self.x = 64 
+        self.y = 70 
+        self.velocidade = 1 
+
+        self.moedas = [[20, 12, "visivel": True], [50, 25,"visivel": True], [120, 10,"visivel": True]]
+
+        pyxel.run(self.update, self.draw) 
+
+    def update(self): 
+        if pyxel.btnp(pyxel.KEY_Q): 
+            pyxel.quit() 
+
+        # Controles do balão
+        if pyxel.btn(pyxel.KEY_LEFT): 
+            self.x -= 2 
+        if pyxel.btn(pyxel.KEY_RIGHT): 
+            self.x += 2 
+
+        # Limites das laterais
+        if self.x < 0:
+            self.x = 0
+        if self.x > 128:
+            self.x = 128
+
+        # Balão sobe
+        if self.y > -32: 
+            self.y -= self.velocidade
+
+        #Colisão das moedas
+        for moeda in self.moedas:
             if moeda["visivel"]:
                 
                 if abs(self.x - moeda["x"]) < 16 and abs(self.y - moeda["y"]) < 16:
-                    moeda["visivel"] = False
+                    moeda["visivel"] = False                        
+
+    def draw(self): 
+        # Fundo azul 
+        pyxel.cls(12) 
+
+
+        # moedas que nao foram coletadas
+        for moeda in self.moedas:
+                    if moeda["visivel"]:
+                         pyxel.blt(self.x, self.y, 0, 0, 0, 16, 16, 0)
+
+
+        # 3 moedas fixas na tela (banco 0, u=0, v=0, tamanho 16x16)
+        pyxel.blt(20, 12, 0, 0, 0, 16, 16, 0)
+        pyxel.blt(50, 25, 0, 0, 0, 16, 16, 0)
+        pyxel.blt(120, 10, 0, 0, 0, 16, 16, 0)
+
+
+        ##pyxel.blt(self.x, self.y, 1, 50, 50, 64, 64, 0)
+
+        #Desenha o balão
+        if self.y > -32:
+            pyxel.blt(self.x, self.y, 0, 67, 0, 29, 35, 0) 
+
+Jogo()
