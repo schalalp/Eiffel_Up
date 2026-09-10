0606ef8296b1360c25bad4c793770b05bc7da27b
schalalp <123514074+schalalp@users.noreply.github.com>
2026-09-10 14:38:07 -0300

    Update eiffel _up.py

--- eiffel _up.py
+++ eiffel _up.py
@@ -5,7 +5,7 @@ class Jogo:
         pyxel.init(160, 120, title="Eiffel Up", fps=10) 
         pyxel.load("eiffel_up.pyxres") 
 
-        # Balão
+        #balão
         self.x = 64 
         self.y = 70 
         self.velocidade = 1 
@@ -23,41 +23,39 @@ class Jogo:
         if pyxel.btnp(pyxel.KEY_Q): 
             pyxel.quit() 
 
-        # Controles do balão
+        # cxontroles do balão
         if pyxel.btn(pyxel.KEY_LEFT): 
             self.x -= 2 
         if pyxel.btn(pyxel.KEY_RIGHT): 
             self.x += 2 
 
-        # Limites das laterais
+        # limites das laterais
         if self.x < 0:
             self.x = 0
         if self.x > 128:
             self.x = 128
 
-        # Balão sobe
+        # balão sobe
         if self.y > -32: 
             self.y -= self.velocidade
 
     
-        # Passamos por cada moeda da lista para checar se o balão encostou nela
+        # se o balão encostou na moeda
         for moeda in self.moedas:
             if moeda["visivel"]:
                 
                 if abs(self.x - moeda["x"]) < 16 and abs(self.y - moeda["y"]) < 16:
-                    moeda["visivel"] = False  # Faz a moeda sumir!
+                    moeda["visivel"] = False 
 
     def draw(self): 
-        # Fundo azul 
         pyxel.cls(12) 
 
-        # --- NOVO: DESENHA APENAS SE FOR VISÍVEL ---
-        # Em vez de linhas fixas, usamos um loop para desenhar as moedas que ainda estão com 'visivel = True'
+        #  desenhar as moedas que ainda estão visiveis
         for moeda in self.moedas:
             if moeda["visivel"]:
                 pyxel.blt(moeda["x"], moeda["y"], 0, 0, 0, 16, 16, 0)
 
-        # Desenha o balão
+        # desenha o balão
         if self.y > -32:
             pyxel.blt(self.x, self.y, 0, 67, 0, 29, 35, 0) 
 
